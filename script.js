6510fc1b2f271c4583827b80882d32c9ecab3d03
titasas <titas.petrauskas@gmail.com>
2022-09-19 21:21:04 +0300

    feat: Add function to create a board

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const container = document.getElementById('grid');
+function createBoard(size) {
+  let board = document.querySelector(".board");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 1; i <= 256; i++) {
-  const cell = document.createElement('div');
-  cell.innerText = i;
-  container.appendChild(cell);
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
+
+createBoard(32);
